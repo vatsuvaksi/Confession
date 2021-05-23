9efb43706b8b65aaa8dd14e56aa752c402deee80
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-05-23 16:28:59 +0530

    Added DB connection

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.static("public"));
@@ -11,9 +11,25 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 
+app.get("/", (req,res)=>{
+    res.render( "home");
+})
+
+app.get("/login", (req,res)=>{
+    res.render( "login");
+})
+app.get("/register", (req,res)=>{
+    res.render( "register");
+})
+
+mongoose.connect("mongodb+srv://vatsuvaksi:password321@cluster0.hmhkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser :true , useUnifiedTopology : true})
+.then(() =>{console.log("DB Connected")})
+.catch((err) =>{console.log(err.message)});
+
 
 
 
 app.listen(3000, ()=>{
     console.log("Listening on 3000");
 });
+
